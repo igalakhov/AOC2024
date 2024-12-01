bea3ab780b2104b841bfea3ea4e188c37f8a34e8
Ivan Galakhov <igalakhov.nyc@gmail.com>
2024-12-01 14:55:11 -0500

    2016 day 4

---
+++ 2016/Day04.hs
@@ -0,0 +1,34 @@
+module Day04 where
+
+import Data.Foldable (Foldable (foldl'), find)
+import Data.Functor (($>))
+import qualified Data.Set as Set
+import Debug.Trace (trace)
+import Grid
+import Linear hiding (trace)
+import Problem (Solution, mkSolution)
+import Text.Parsec (char, many, many1, newline, optional, string, (<|>), spaces, letter)
+import Text.Parsec.Number (int)
+import Text.Parsec.Text (Parser)
+import Text.Parsec.Token (GenTokenParser(whiteSpace))
+import Data.List.Split (chunksOf)
+import qualified Data.Map as Map
+import Data.List (sort, elemIndex)
+
+-- (chars, checksum)
+type Input = [(String, Int, String)]
+
+parser :: Parser Input
+parser = many $ ((,,) <$> (filter ('-' /=) <$> many (letter <|> char '-')) <*> int <* char '[' <*> many letter <* char ']') <* newline
+
+part1 input = sum $ check <$> input
+    where
+        check (code, rid, cs) = if cs == take (length cs) (snd <$> sort  ((\(x, y) -> (-y, x)) <$> Map.toList (foldr (\e m -> Map.insertWith (+) e 1 m) Map.empty code))) then rid else 0
+
+part2 input = fst <$> find ((==) "northpoleobjectstorage" . snd) ((\(code, rid, _) -> (rid, shiftChar rid <$> code)) <$> input)
+    where
+        shiftChar amt chr = let Just idx = elemIndex chr alphabet in alphabet !! ((idx + amt) `mod` length alphabet)
+            where alphabet = ['a'..'z']
+
+solve :: Solution
+solve = mkSolution parser (part1, part2)
--- 2016/Year2016.hs
+++ 2016/Year2016.hs
@@ -4,6 +4,7 @@ import qualified Data.Map as Map
 import qualified Day01
 import qualified Day02
 import qualified Day03
+import qualified Day04
 import Problem (Solutions)
 
 solutions :: Solutions
@@ -11,5 +12,6 @@ solutions =
   Map.fromList
     [ (1, Day01.solve),
       (2, Day02.solve),
-      (3, Day03.solve)
+      (3, Day03.solve),
+      (4, Day04.solve)
     ]
