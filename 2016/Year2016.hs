527f8f3305d225eaff5304231ff9e4a60ee28ecd
Ivan Galakhov <igalakhov.nyc@gmail.com>
2024-12-01 20:19:08 -0500

    other files as well

--- 2016/Year2016.hs
+++ 2016/Year2016.hs
@@ -5,6 +5,8 @@ import qualified Day01
 import qualified Day02
 import qualified Day03
 import qualified Day04
+import qualified Day05
+import qualified Day06
 import Problem (Solutions)
 
 solutions :: Solutions
@@ -13,5 +15,7 @@ solutions =
     [ (1, Day01.solve),
       (2, Day02.solve),
       (3, Day03.solve),
-      (4, Day04.solve)
+      (4, Day04.solve),
+      (5, Day05.solve),
+      (6, Day06.solve)
     ]
