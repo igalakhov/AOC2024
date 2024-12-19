c31491f3eb29d89329666b84d17a99f8640d4fab
Ivan Galakhov <igalakhov.nyc@gmail.com>
2024-12-19 17:27:43 -0500

    2024 day 18

---
+++ 2024/Day18.hs
@@ -0,0 +1,57 @@
+module Day18 where
+
+import Algorithm.Search (bfs, dfs)
+import Control.Applicative (ZipList (ZipList))
+import Control.Monad
+import Control.Monad.Trans.State.Lazy
+import Data.Either
+import Data.Foldable (Foldable (foldl'), find)
+import Data.Function ((&))
+import Data.Functor (($>))
+import Data.List
+import Data.List.Split (chunksOf)
+import qualified Data.Map as Map
+import Data.Maybe (catMaybes, isJust, listToMaybe, mapMaybe, isNothing)
+import qualified Data.Set as Set
+import Data.String (IsString (fromString))
+import Data.Tuple
+import Debug.Trace (trace)
+import GHC.Integer (integerToInt)
+import Grid
+import Linear
+import Problem (Solution, mkSolution)
+import Text.Parsec (anyChar, char, eof, letter, many, many1, newline, oneOf, optional, sepBy, sepBy1, sepEndBy1, space, spaces, string, try, (<|>))
+import Text.Parsec.Number (int)
+import Text.Parsec.Text (Parser)
+import Text.Parsec.Token (GenTokenParser (whiteSpace))
+
+type Input = [(Int, Int)]
+
+parser :: Parser Input
+parser = many $ (,) <$> int <* char ',' <*> int <* spaces
+
+search map =
+  bfs
+    ( \(x, y) ->
+        [ (x, y)
+          | (x, y) <- [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)],
+            0 <= x,
+            x <= 70,
+            0 <= y,
+            y <= 70,
+            Set.notMember (x, y) map
+        ]
+    )
+    (== (70, 70))
+    (0, 0)
+
+part1 = (length <$>) . search . Set.fromList . take 1024
+
+part2 pts = 
+  fmap (\(_, (x, y)) -> show x ++ "," ++ show y) $ 
+  find (isNothing . fst) $ 
+  (\x -> ((search . Set.fromList) x, last x)) <$>
+  (take <$> [1024 ..] <*> pure pts)
+
+solve :: Solution
+solve = mkSolution parser (part1, part2)
--- 2024/Year2024.hs
+++ 2024/Year2024.hs
@@ -14,22 +14,37 @@ import qualified Day11
 import qualified Day12
 import qualified Day13
 import qualified Day14
+import qualified Day18
 import Problem (Solutions)
 
 solutions :: Solutions
 solutions =
-  Map.fromList
-    [ (1, Day01.solve),
-      (2, Day02.solve),
-      (3, Day03.solve),
-      (4, Day04.solve),
-      (5, Day05.solve),
-      (7, Day07.solve),
-      (8, Day08.solve),
-      (9, Day09.solve),
-      (10, Day10.solve),
-      (11, Day11.solve),
-      (12, Day12.solve),
-      (13, Day13.solve),
-      (14, Day14.solve)
-    ]
+  Map.fromList $
+    zip
+      [1 ..]
+      [ Day01.solve,
+        Day02.solve,
+        Day03.solve,
+        Day04.solve,
+        Day05.solve,
+        undefined,
+        Day07.solve,
+        Day08.solve,
+        Day09.solve,
+        Day10.solve,
+        Day11.solve,
+        Day12.solve,
+        Day13.solve,
+        Day14.solve,
+        undefined,
+        undefined,
+        undefined,
+        Day18.solve,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined
+      ]
