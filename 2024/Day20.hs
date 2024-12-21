7506033fd631b061a9fd0fad5e3a21a19ea70ae5
Ivan Galakhov <igalakhov.nyc@gmail.com>
2024-12-20 20:18:18 -0500

    cleanup 2024 day 20

--- 2024/Day20.hs
+++ 2024/Day20.hs
@@ -1,26 +1,11 @@
 module Day20 where
 
-import Control.Monad (guard)
 import Control.Monad.Trans.State.Lazy
-import Data.Either
-import Data.Foldable (Foldable (foldl'), find)
-import Data.Function ((&))
-import Data.Functor (($>))
-import Data.List
-import Data.List.Split (chunksOf)
 import qualified Data.Map as Map
-import Data.Maybe (catMaybes, isJust, listToMaybe, mapMaybe)
 import qualified Data.Set as Set
-import Data.String (IsString (fromString))
-import Data.Tuple
-import Debug.Trace (trace)
-import GHC.Integer (integerToInt)
-import Grid
-import Problem (Solution, mkSolution)
-import Text.Parsec (anyChar, char, eof, letter, many, many1, newline, oneOf, optional, sepBy, sepBy1, sepEndBy1, space, spaces, string, try, (<|>))
-import Text.Parsec.Number (int)
-import Text.Parsec.Text (Parser)
-import Text.Parsec.Token (GenTokenParser (whiteSpace))
+import Problem
+import Text.Parsec
+import Text.Parsec.Text
 
 -- (walls, start, end)
 type Input = (Set.Set (Int, Int), (Int, Int), (Int, Int))
