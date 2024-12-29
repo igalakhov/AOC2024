89447165816a47370868ed723a7ffab7da57cca4
Ivan Galakhov <igalakhov.nyc@gmail.com>
2024-12-29 18:11:46 -0500

    2024 day 4

--- 2024/Day04.hs
+++ 2024/Day04.hs
@@ -1,40 +1,42 @@
 module Day04 where
 
-import Data.Either
-import Data.Foldable (Foldable (foldl'), find)
-import Data.Function ((&))
-import Data.Functor (($>))
 import Data.List
-import Data.List.Split (chunksOf)
 import qualified Data.Map as Map
-import Data.Maybe (catMaybes)
-import qualified Data.Set as Set
-import Data.String (IsString (fromString))
-import Data.Tuple
-import Debug.Trace (trace)
-import GHC.Integer (integerToInt)
-import Grid
-import Problem (Solution, mkSolution)
-import Text.Parsec (anyChar, char, eof, letter, many, many1, newline, oneOf, optional, sepBy1, sepEndBy1, space, spaces, string, try, (<|>))
-import Text.Parsec.Number (int)
-import Text.Parsec.Text (Parser)
-import Text.Parsec.Token (GenTokenParser (whiteSpace))
+import Data.Maybe
+import Linear
+import Problem
+import Text.Parsec
+import Text.Parsec.Text
 
-type Input = [[Char]]
+type Input = Map.Map (V2 Int) Char
 
 parser :: Parser Input
-parser = many1 $ (many1 (oneOf "XMAS.")) <* (() <$ newline <|> eof)
-
-part1 input = sum . take 4 $ stuff <$> iterate transpose input
+parser = (mkGrid <$>) . many1 $ (many1 (oneOf "XMAS.")) <* optional newline
   where
-    stuff input =
-      let diags1 = (transpose . diags) input
-          diags2 = drop 1 $ transpose . reverse $ diags (reverse (reverse <$> input))
-       in length $ filter (isPrefixOf "XMAS") ((diags1 ++ diags2 ++ input) >>= tails)
-    diags [] = []
-    diags (x : xs) = x : (drop 1 <$> diags xs)
+    mkGrid l =
+      Map.fromList
+        [ (V2 i j, c)
+          | (i, r) <- zip [1 ..] l,
+            (j, c) <- zip [1 ..] r
+        ]
 
-part2 = id
+part1 grid =
+  length . filter (== "XMAS") $
+    [ catMaybes . take 4 $ (`Map.lookup` grid) <$> iterate (+ d) c
+      | c <- Map.keys grid,
+        d <-
+          [ V2 x y
+            | x <- [-1, 0, 1],
+              y <- [-1, 0, 1],
+              x /= 0 || y /= 0
+          ]
+    ]
+
+part2 grid =
+  length . filter (== ["A", "MS", "MS"]) $
+    [ sort . catMaybes . ((`Map.lookup` grid) . (+ c) <$>) <$> [[V2 0 0], [V2 (-1) (-1), V2 1 1], [V2 (-1) 1, V2 1 (-1)]]
+      | c <- Map.keys grid
+    ]
 
 solve :: Solution
 solve = mkSolution parser (part1, part2)
